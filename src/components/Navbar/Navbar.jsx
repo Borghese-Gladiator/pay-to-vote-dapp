786c00f87804f6299d4f05981c463979b0ca9352
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-01-27 03:10:09 -0500

    Working navbar (looks to be improved)

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { Link, Box, Flex, Text, Button, Stack } from "@chakra-ui/react";
+import { Link, Box, Flex, Text, Button, Stack, Icon, IconButton } from "@chakra-ui/react";
 import Logo from "./Logo";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
 import { FaGithubSquare } from "react-icons/fa";
+import { AiOutlineClose } from "react-icons/ai";
+import { GoMarkGithub } from "react-icons/go";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const NavBar = (props) => {
   const [isOpen, setIsOpen] = React.useState(false);
@@ -11,7 +15,7 @@ const NavBar = (props) => {
   return (
     <NavBarContainer {...props}>
       <Logo
-        w="100px"
+        w="150px"
         color={["white", "white", "primary.500", "primary.500"]}
       />
       <MenuToggle toggle={toggle} isOpen={isOpen} />
@@ -20,43 +24,11 @@ const NavBar = (props) => {
   );
 };
 
-const CloseIcon = () => (
-  <svg width="24" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
-    <title>Close</title>
-    <path
-      fill="white"
-      d="M9.00023 7.58599L13.9502 2.63599L15.3642 4.04999L10.4142 8.99999L15.3642 13.95L13.9502 15.364L9.00023 10.414L4.05023 15.364L2.63623 13.95L7.58623 8.99999L2.63623 4.04999L4.05023 2.63599L9.00023 7.58599Z"
-    />
-  </svg>
-);
-
-const MenuIcon = () => (
-  <svg
-    width="24px"
-    viewBox="0 0 20 20"
-    xmlns="http://www.w3.org/2000/svg"
-    fill="white"
-  >
-    <title>Menu</title>
-    <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-  </svg>
-);
-
 const MenuToggle = ({ toggle, isOpen }) => {
   return (
     <Box display={{ base: "block", md: "none" }} onClick={toggle}>
-      {isOpen ? <CloseIcon /> : <MenuIcon />}
+      {isOpen ? <Icon as={AiOutlineClose} w={8} h={8} /> : <Icon as={GiHamburgerMenu} w={8} h={8} />}
     </Box>
-  );
-};
-
-const MenuItem = ({ children, isLast, to = "/", ...rest }) => {
-  return (
-    <Link href={to}>
-      <Text display="block" {...rest}>
-        {children}
-      </Text>
-    </Link>
   );
 };
 
@@ -71,28 +43,30 @@ const MenuLinks = ({ isOpen }) => {
         align="center"
         justify={["center", "space-between", "flex-end", "flex-end"]}
         direction={["column", "row", "row", "row"]}
-        pt={[4, 4, 0, 0]}
+        pt={[4, 2, 0, 0]}
       >
-        <MenuItem to="/">Home</MenuItem>
-        <MenuItem to="/how">How It works </MenuItem>
-        <MenuItem to="/faetures">Features </MenuItem>
-        <MenuItem to="/pricing">Pricing </MenuItem>
-        <MenuItem to="/signup" isLast>
-          <Button
-            size="sm"
-            rounded="md"
-            color={["primary.500", "primary.500", "white", "white"]}
-            bg={["white", "white", "primary.500", "primary.500"]}
-            _hover={{
-              bg: ["primary.100", "primary.100", "primary.600", "primary.600"]
-            }}
+        <Link
+            href='https://chakra-ui.com'
+            target="_blank"
+            rel="noopener noreferrer"
+            isExternal
           >
-            Create Account
-          </Button>
-        </MenuItem>
-        <MenuItem>
-        <FaGithubSquare />
-        </MenuItem>
+            Etherscan
+            <ExternalLinkIcon w={8} h={8} />
+          </Link>
+          <Link
+            href='https://chakra-ui.com'
+            target="_blank"
+            rel="noopener noreferrer"
+            isExternal
+          >
+            <IconButton
+              colorScheme='gray'
+              aria-label='Go to GitHub link'
+              size='lg'
+              icon={<Icon as={GoMarkGithub} w={8} h={8} />}
+            />
+          </Link>
       </Stack>
     </Box>
   );
@@ -118,3 +92,4 @@ const NavBarContainer = ({ children, ...props }) => {
 };
 
 export default NavBar;
+// <Icon as={FaGithubSquare} w={8} h={8} color='gray.400' />
