29418515a9c858b0463ee2a45415fab9f14b432c
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-01-28 15:13:46 -0500

    Load env variables for greeterAddress

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
+// Load smart contracts
+import { ethers } from 'ethers'
+import Greeter from '../src/artifacts/contracts/Greeter.sol/Greeter.json'
+import SimpleAuction from '../src/artifacts/contracts/SimpleAuction.sol/SimpleAuction.json'
+// Frontend
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import Navbar from "../src/components/Navbar";
@@ -31,47 +36,95 @@ const transactions = [
   { date: new Date(), username: "User 1", contribution: 10, link: "https://etherscan.io/tx/0x88f6d02ef407d84a6a558d69a302c21c73d32b8c0ff5811aef253e3f25824563"},
 ];
 
-export default function Home() {
-  // Detect Ethereum Provider is present
+export default function Home({ greeterAddress, simpleAuctionAddress }) {
+  // request access to the user's MetaMask account
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  // call the smart contract, read the current greeting value
+  async function fetchGreeting() {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log('data: ', data)
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }
+  }
+
+  // call the smart contract, send an update
+  async function updateGreeting() {
+    if (!greeting) return
+    if (typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+      const transaction = await contract.setGreeting(greeting)
+      await transaction.wait()
+      fetchGreeting()
+    }
+  }
+  
+  // Detect Ethereum Provider is present, render MetaMask not detected
   if (typeof window !== "undefined" && typeof window.ethereum === "undefined") {
     return <NoWalletDetected />;
   }
-  else {
-    return (
-      <div>
-        <Head>
-          <title>Pay2Vote</title>
-          <meta name="description" content="DApp for users to pay Test Ether to vote and highest voter takes all" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-        <main style={{ height: "80vh" }}>
-          <Navbar />
-          <Container h={'100%'} maxW='container.lg'>
-            <Grid
-              h={'100%'}
-              templateRows='repeat(2, 1fr)'
-              templateColumns='repeat(2, 1fr)'
-              gap={4}
-            >
-              <GridItem colSpan={1}>
-                <LeaderTable players={players} />
-              </GridItem>
-              <GridItem colSpan={1}>
-                <UserProfile playerObj={playerObj} />
-              </GridItem>
-              <GridItem colSpan={2}>
-                <UserTxnTable transactions={transactions} />
-              </GridItem>
-            </Grid>
-          </Container>
-        </main>
-  
-        <footer>
-        </footer>
-      </div>
-    )
+
+  // Everything is loaded, render application
+  return (
+    <div>
+      <Head>
+        <title>Pay2Vote</title>
+        <meta name="description" content="DApp for users to pay Test Ether to vote and highest voter takes all" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main style={{ height: "80vh" }}>
+        <Navbar />
+        <Container h={'100%'} maxW='container.lg'>
+          <Grid
+            h={'100%'}
+            templateRows='repeat(3, 1fr)'
+            templateColumns='repeat(2, 1fr)'
+            gap={4}
+          >
+            <GridItem colSpan={2}>
+            <button onClick={fetchGreeting}>Fetch Greeting</button>
+            <button onClick={updateGreeting}>Set Greeting</button>
+            <input onChange={e => setGreeting(e.target.value)} placeholder="Set greeting" />
+            </GridItem>
+            <GridItem colSpan={1}>
+              <LeaderTable players={players} />
+            </GridItem>
+            <GridItem colSpan={1}>
+              <UserProfile playerObj={playerObj} />
+            </GridItem>
+            <GridItem colSpan={2}>
+              <UserTxnTable transactions={transactions} />
+            </GridItem>
+          </Grid>
+        </Container>
+      </main>
+
+      <footer>
+      </footer>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      greeterAddress: process.env.GREETER_DEPLOYED_ADDRESS,
+      simpleAuctionAddress: process.env.SIMPLE_AUCTION_DEPLOYED_ADDRESS
+    }
   }
 }
+
 /*
 width={[
   '100%', // 0-30em
