921df237b45e724b3b783b131ca8e822ded1efe8
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-01-28 00:37:05 -0500

    Write deploy script & Add SimpleAuction contract

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,13 @@
+require('dotenv').config()
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+
+// Value of Date: Wed Aug 22 16:37:36 CST 2012
+const dateUINT = 1345619256308
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -15,11 +19,19 @@ async function main() {
 
   // We get the contract to deploy
   const Greeter = await hre.ethers.getContractFactory("Greeter");
+  const SimpleAuction = await hre.ethers.getContractFactory("SimpleAuction");
+  
   const greeter = await Greeter.deploy("Hello, Hardhat!");
-
+  const simpleAuction = await SimpleAuction.deploy(
+      dateUINT,
+      process.env.METAMASK_WALLET_ACCOUNT_ADDRESS
+    );
+  
   await greeter.deployed();
+  await simpleAuction.deployed();
 
   console.log("Greeter deployed to:", greeter.address);
+  console.log("Simple Auction deployed to:", simpleAuction.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
