04ae20716ad9ef2c044567169924966ee66b3015
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-01-28 00:27:46 -0500

    Add hardhat & setup dotenv

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+require('dotenv').config()
+require("@nomiclabs/hardhat-waffle");
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  defaultNetwork: "hardhat",
+  paths: {
+    artifacts: './src/artifacts',
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_PROJECT_ID}`,
+      accounts: [`0x${process.env.METAMASK_WALLET_PRIVATE_KEY}`]
+    }
+  },
+  solidity: "0.8.4",
+};
