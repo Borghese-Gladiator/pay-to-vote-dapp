5a7c33b474401421558a97d981736970c192e4fb
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-01-28 15:20:06 -0500

    Move Head to RootLayout

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,7 @@ import Greeter from '../src/artifacts/contracts/Greeter.sol/Greeter.json'
 import SimpleAuction from '../src/artifacts/contracts/SimpleAuction.sol/SimpleAuction.json'
 // Frontend
 import { useEffect, useState } from 'react';
-import Head from 'next/head';
-import Navbar from "../src/components/Navbar";
+import RootLayout from "../src/components/_layouts/RootLayout";
 import NoWalletDetected from "../src/components/NoWalletDetected";
 import LeaderTable from "../src/components/LeaderTable";
 import UserProfile from "../src/components/UserProfile";
@@ -30,10 +29,10 @@ const playerObj = {
 };
 
 const transactions = [
-  { date: new Date('1995-12-17T03:24:00'), username: "User 1", contribution: 10, link: "https://etherscan.io/tx/0x88f6d02ef407d84a6a558d69a302c21c73d32b8c0ff5811aef253e3f25824563"},
-  { date: new Date('1997-12-17T03:24:00'), username: "User 1", contribution: 10, link: "https://etherscan.io/tx/0x88f6d02ef407d84a6a558d69a302c21c73d32b8c0ff5811aef253e3f25824563"},
-  { date: new Date('2000-12-17T03:24:00'), username: "User 1", contribution: 10, link: "https://etherscan.io/tx/0x88f6d02ef407d84a6a558d69a302c21c73d32b8c0ff5811aef253e3f25824563"},
-  { date: new Date(), username: "User 1", contribution: 10, link: "https://etherscan.io/tx/0x88f6d02ef407d84a6a558d69a302c21c73d32b8c0ff5811aef253e3f25824563"},
+  { date: new Date('1995-12-17T03:24:00'), username: "User 1", contribution: 10, link: "https://etherscan.io/tx/0x88f6d02ef407d84a6a558d69a302c21c73d32b8c0ff5811aef253e3f25824563" },
+  { date: new Date('1997-12-17T03:24:00'), username: "User 1", contribution: 10, link: "https://etherscan.io/tx/0x88f6d02ef407d84a6a558d69a302c21c73d32b8c0ff5811aef253e3f25824563" },
+  { date: new Date('2000-12-17T03:24:00'), username: "User 1", contribution: 10, link: "https://etherscan.io/tx/0x88f6d02ef407d84a6a558d69a302c21c73d32b8c0ff5811aef253e3f25824563" },
+  { date: new Date(), username: "User 1", contribution: 10, link: "https://etherscan.io/tx/0x88f6d02ef407d84a6a558d69a302c21c73d32b8c0ff5811aef253e3f25824563" },
 ];
 
 export default function Home({ greeterAddress, simpleAuctionAddress }) {
@@ -69,50 +68,43 @@ export default function Home({ greeterAddress, simpleAuctionAddress }) {
       fetchGreeting()
     }
   }
-  
+
   // Detect Ethereum Provider is present, render MetaMask not detected
   if (typeof window !== "undefined" && typeof window.ethereum === "undefined") {
-    return <NoWalletDetected />;
+    return (
+      <RootLayout>
+        <NoWalletDetected />
+      </RootLayout>
+    )
   }
 
   // Everything is loaded, render application
   return (
-    <div>
-      <Head>
-        <title>Pay2Vote</title>
-        <meta name="description" content="DApp for users to pay Test Ether to vote and highest voter takes all" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main style={{ height: "80vh" }}>
-        <Navbar />
-        <Container h={'100%'} maxW='container.lg'>
-          <Grid
-            h={'100%'}
-            templateRows='repeat(3, 1fr)'
-            templateColumns='repeat(2, 1fr)'
-            gap={4}
-          >
-            <GridItem colSpan={2}>
+    <RootLayout>
+      <Container h={'100%'} maxW='container.lg'>
+        <Grid
+          h={'100%'}
+          templateRows='repeat(3, 1fr)'
+          templateColumns='repeat(2, 1fr)'
+          gap={2}
+        >
+          <GridItem colSpan={2}>
             <button onClick={fetchGreeting}>Fetch Greeting</button>
             <button onClick={updateGreeting}>Set Greeting</button>
             <input onChange={e => setGreeting(e.target.value)} placeholder="Set greeting" />
-            </GridItem>
-            <GridItem colSpan={1}>
-              <LeaderTable players={players} />
-            </GridItem>
-            <GridItem colSpan={1}>
-              <UserProfile playerObj={playerObj} />
-            </GridItem>
-            <GridItem colSpan={2}>
-              <UserTxnTable transactions={transactions} />
-            </GridItem>
-          </Grid>
-        </Container>
-      </main>
-
-      <footer>
-      </footer>
-    </div>
+          </GridItem>
+          <GridItem colSpan={1}>
+            <LeaderTable players={players} />
+          </GridItem>
+          <GridItem colSpan={1}>
+            <UserProfile playerObj={playerObj} />
+          </GridItem>
+          <GridItem colSpan={2}>
+            <UserTxnTable transactions={transactions} />
+          </GridItem>
+        </Grid>
+      </Container>
+    </RootLayout>
   )
 }
 
---
+++ src/components/_layouts/RootLayout.jsx
@@ -0,0 +1,23 @@
+import Head from 'next/head';
+import Navbar from "../Navbar";
+import {
+  Flex,
+} from '@chakra-ui/react';
+
+export default function RootLayout({ children }) {
+  return (
+    <div>
+      <Head>
+        <title>Pay2Vote</title>
+        <meta name="description" content="DApp for users to pay Test Ether to vote and highest voter takes all" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main style={{ height: "80vh" }}>
+        <Navbar />
+        {children}
+      </main>
+      <footer>
+      </footer>
+    </div>
+  )
+}
