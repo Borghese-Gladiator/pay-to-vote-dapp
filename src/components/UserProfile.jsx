48e221e4acbe3cdb979d751c5239c3b4a07ee331
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-01-28 16:09:42 -0500

    Add box and center content

--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,15 +1,29 @@
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from "./ErrorFallback";
 import {
+  Box,
   VStack,
+  Flex,
   Text
 } from '@chakra-ui/react';
 
 export default function UserProfile({ playerObj }) {
-  const {  username, highest, rank } = playerObj;
+  const { username, highest, rank } = playerObj;
   return (
-    <VStack>
-      <Text fontSize='4xl'>Hi {username}</Text>
-      <Text fontSize='xl'>Current Rank: <Text fontSize='2xl' as='span'>{rank}</Text></Text>
-      <Text fontSize='xl'>Current Highest: <Text fontSize='2xl' as='span'>${highest}</Text></Text>
-    </VStack>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => setGreeting(defaultGreeting)}
+    >
+      <VStack
+        justify={"center"}
+        alignItems="center"
+        borderWidth={1}
+        h={"100%"}
+      >
+        <Text fontSize='4xl'>Hi {username}</Text>
+        <Text fontSize='xl'>Current Rank: <Text fontSize='2xl' as='span'>{rank}</Text></Text>
+        <Text fontSize='xl'>Current Highest: <Text fontSize='2xl' as='span'>${highest}</Text></Text>
+      </VStack>
+    </ErrorBoundary>
   )
 }
