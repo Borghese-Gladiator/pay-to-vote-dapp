2985166a36f920af552cb6ff3201ec3f49a33be3
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-01-29 01:00:53 -0500

    Move contract calls to GreeterDisplay

--- pages/index.js
+++ pages/index.js
@@ -37,43 +37,6 @@ const transactions = [
 ];
 
 export default function Home({ greeterAddress, simpleAuctionAddress }) {
-  const [greeting, setGreeting] = useState();
-  
-  // request access to the user's MetaMask account
-  async function requestAccount() {
-    await window.ethereum.request({ method: 'eth_requestAccounts' });
-  }
-
-  // call the smart contract, read the current greeting value
-  async function fetchGreeting() {
-    if (typeof window !== "undefined" && typeof window.ethereum !== 'undefined') {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
-      try {
-        const data = await contract.greet()
-        console.log('data: ', data)
-        setGreeting(data);
-      } catch (err) {
-        console.log("Error: ", err)
-      }
-    }
-  }
-
-  // call the smart contract, send an update
-  async function updateGreeting(e) {
-    e.preventDefault();
-    const newGreeting = e.target.greeting.value;
-    if (!greeting) return
-    if (typeof window !== "undefined" && typeof window.ethereum !== 'undefined') {
-      await requestAccount()
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      const signer = provider.getSigner()
-      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
-      const transaction = await contract.setGreeting(newGreeting)
-      await transaction.wait()
-      fetchGreeting()
-    }
-  }
 
   // Detect Ethereum Provider is present, render MetaMask not detected
   if (typeof window !== "undefined" && typeof window.ethereum === "undefined") {
@@ -84,7 +47,6 @@ export default function Home({ greeterAddress, simpleAuctionAddress }) {
     )
   }
 
-  fetchGreeting();
   // Everything is loaded, render application
   return (
     <RootLayout>
@@ -96,7 +58,7 @@ export default function Home({ greeterAddress, simpleAuctionAddress }) {
           gap={2}
         >
           <GridItem colSpan={2}>
-            <GreeterDisplay greeting={greeting} setGreeting={setGreeting} handleSubmitGreeting={updateGreeting} />
+            <GreeterDisplay />
           </GridItem>
           <GridItem colSpan={1}>
             <LeaderTable players={players} />
--- src/components/GreeterDisplay.jsx
+++ src/components/GreeterDisplay.jsx
@@ -12,8 +12,48 @@ import {
 } from '@chakra-ui/react';
 
 const textOneLineStyle = { whiteSpace: "nowrap" }
+const defaultGreeting = "";
 
-export default function GreeterDisplay({ greeting, setGreeting, handleSubmitGreeting }) {
+export default function GreeterDisplay({ greeterAddress }) {
+  const [greeting, setGreeting] = useState();
+  
+  // request access to the user's MetaMask account
+  async function requestAccount() {
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+  }
+
+  // call the smart contract, read the current greeting value
+  async function fetchGreeting() {
+    if (typeof window !== "undefined" && typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider)
+      try {
+        const data = await contract.greet()
+        console.log('data: ', data)
+        return data;
+      } catch (err) {
+        console.log("Error: ", err)
+      }
+    }
+  }
+
+  // call the smart contract, send an update
+  async function updateGreeting(e) {
+    e.preventDefault();
+    const newGreeting = e.target.greeting.value;
+    if (!greeting) return
+    if (typeof window !== "undefined" && typeof window.ethereum !== 'undefined') {
+      await requestAccount()
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer)
+      const transaction = await contract.setGreeting(newGreeting)
+      await transaction.wait()
+      setGreeting(fetchGreeting());
+    }
+  }
+
+  setGreeting(fetchGreeting());
   return (
     <ErrorBoundary
       FallbackComponent={ErrorFallback}
@@ -27,7 +67,7 @@ export default function GreeterDisplay({ greeting, setGreeting, handleSubmitGree
         <Stack>
           <Heading>Greeter</Heading>
           <Text p>Current greeting: {greeting}</Text>
-          <form onSubmit={handleSubmitGreeting}>
+          <form onSubmit={updateGreeting}>
             <FormControl isRequired>
               <Flex alignItems="center" m={1}>
                 <Text style={textOneLineStyle}>Set Greeting</Text>
