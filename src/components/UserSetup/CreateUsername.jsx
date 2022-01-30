0b820cb311d6f8c9d6c74ee2afab61488813f030
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-01-29 19:07:21 -0500

    Implemented message signing

--- src/components/UserSetup/CreateUsername.jsx
+++ src/components/UserSetup/CreateUsername.jsx
@@ -1,39 +1,34 @@
+import { useState } from "react";
+import useAsync from "../../hooks/useAsync";
 import {
   Box,
-  Stack,
-  Heading,
   Input,
   Text,
   Button,
   Flex,
-  FormControl
+  Spinner
 } from '@chakra-ui/react';
 
 const textOneLineStyle = { whiteSpace: "nowrap" }
 
 export default function CreateUsername({ userInfo, setUserInfo }) {
-  async function updateUsername(e) {
-    if (typeof window !== "undefined") {
-      e.preventDefault();
-      const name = e.target.username.name;
-      // Tell user to enter name
-      const message = "Hello from Ethereum Stack Exchange!";
-      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-      const account = accounts[0];
-      const signature = await window.ethereum.request({ method: 'personal_sign', params: [message, account] });
+  const [username, setUsername] = useState('')
+  const handleChange = (event) => setUsername(event.target.value);
 
+  const { execute, status, value, error } = useAsync(updateUsername, false);
+  async function updateUsername() {
+    try {
+      const message = `Hello! I will sign this message with name: ${username}`;
+      const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+      const account = accounts[0]; // MetaMask gets current account
+      const signature = await window.ethereum.request({ method: 'personal_sign', params: [message, account] });
       setUserInfo({
         ...userInfo,
-        name: name
+        name: username
       });
+    } catch (e) {
+      throw new Error(e.message);
     }
-    /*
-    // Create a SHA3 hash of the message 'Apples'
-    const messageHash = web3.sha3('Apples');
-
-    // Signs the messageHash with a given account
-    const signature = await web3.eth.personal.sign(messageHash, web3.eth.defaultAccount);
-    */
   }
 
   return (
@@ -42,19 +37,26 @@ export default function CreateUsername({ userInfo, setUserInfo }) {
       mb={1}
       p={3}
     >
-      <Stack>
-        <Heading>Set Username</Heading>
-        <form onSubmit={updateUsername}>
-          <FormControl isRequired>
-            <Flex alignItems="center" m={1}>
-              <Text style={textOneLineStyle}>Set Greeting</Text>
-              <Box pl={1} />
-              <Input name="username" placeholder={"Winged Victory of Samothrace"} size="lg" />
-              <Button type="submit">Submit</Button>
-            </Flex>
-          </FormControl>
-        </form>
-      </Stack>
+      {status === "idle" && <div>Waiting to set username by using Web3 Message signing given user address...</div>}
+      {status === "success" && <div>{value}</div>}
+      {status === "error" && <div>{error}</div>}
+      <Flex alignItems="center" m={1}>
+        <Text style={textOneLineStyle}>Set Username</Text>
+        <Box pl={1} />
+        <Input
+          value={value}
+          onChange={handleChange}
+          placeholder={"Winged Victory of Samothrace"} size="lg" />
+        <Button onClick={execute} disabled={status === "pending"}>Submit</Button>
+        {status === "pending"
+          && <Spinner
+            thickness='4px'
+            speed='0.65s'
+            emptyColor='gray.200'
+            color='blue.500'
+            size='xl'
+          />}
+      </Flex>
     </Box>
   )
 }
