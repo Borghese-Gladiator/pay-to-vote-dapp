5768c6d138f7949003ea9eab93fa77e2cea44a50
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-02-10 05:02:29 -0500

    Change if statements to switch statement to clarify code & remove comments

--- pages/api/profile.js
+++ pages/api/profile.js
@@ -6,28 +6,30 @@
 import { connectToDatabase } from "../../src/mongodb";
 
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    // Process a GET request
-    const profile = await db
-      .collection("voter_list")
-      .findOne({ address: address });
-    
-    res.status(200).json(profile);
-  } else if (req.method === 'POST') {
-    // Process a POST request
-    const { address, username } = req;
-    
-    // Upsert (update if present, insert if not) MongoDB default operation
-    const response = await db
-      .collection("voter_list")
-      .updateOne(
-        { address: address },
-        { $set: { username: username } },
-        { upsert: true }
-      );
-    res.status(200).json({ response: response, message: "Success! Username created"});
-  } else {
-    // Handle any other HTTP method
-    res.status(405).json({ error: "Only GET/POST are supported" });
+  const { method } = req;
+  const { db } = await connectToDatabase();
+
+  switch (method) {
+    case 'GET':
+      const profile = await db
+        .collection("voter_list")
+        .findOne({ address: address });
+      res.status(200).json(profile);
+      break
+    case 'POST':
+      const { address, username } = req;
+      // Upsert (update if present, insert if not) MongoDB default operation
+      const response = await db
+        .collection("voter_list")
+        .updateOne(
+          { address: address },
+          { $set: { username: username } },
+          { upsert: true }
+        );
+      res.status(200).json({ response: response, message: "Success! Username created"});
+      break
+    default:
+      res.setHeader('Allow', ['GET', 'POST'])
+      res.status(405).end(`Method ${method} Not Allowed`)
   }
 }
--- pages/api/vote.js
+++ pages/api/vote.js
@@ -1,43 +1,46 @@
 /**
- * Sets vote
+ * VOTING API ENDPOINT
+ * sets vote
  */
 import { connectToDatabase } from "../../src/mongodb";
 import { setVote } from "../../src/contractUtils";
 
 const contractAddress = process.env.CUSTOM_VOTING_DEPLOYED_ADDRESS;
 export default async function handler(req, res) {
-  if (req.method === 'GET') {
-    // Process a GET request
-    const profile = await db
-      .collection("voter_list")
-      .findOne({ address: address });
-    
-    res.status(200).json(profile);
-  } else if (req.method === 'POST') {
-    // Process a POST request
-    const { address, contribution } = req;
+  const { method } = req;
+  const { db } = await connectToDatabase();
 
-    const transaction = await setVote(contractAddress, address, username, contribution);
-    // from transaction - need error info, need hash for txn
-    console.log(transaction);
-    if (!transaction) {
-      res.status(500).json({ error: "Contract failed to save vote"});
-    }
-    const transactionObject = {
-      date: new Date().toISOString(),
-      txnHash: transaction,
-      contribution
-    }
-    const response = await db
-      .collection("series_list")
-      .updateOne(
-        { address: address },
-        { $set: { highestContribution: contribution } },
-        { $push: { transactionList: transactionObject }}
-      )
-    res.status(200).json({ response: response, message: "Success! New vote saved"});
-  } else {
-    // Handle any other HTTP method
-    res.status(405).json({ error: "Only GET/POST are supported" });
+  switch (method) {
+    case 'GET':
+      const profile = await db
+        .collection("voter_list")
+        .findOne({ address: address });
+      res.status(200).json(profile);
+      break
+    case 'POST':
+      const { address, contribution } = req;
+      const transaction = await setVote(contractAddress, address, username, contribution);
+      // from transaction - need error info, need hash for txn
+      console.log(transaction);
+      if (!transaction) {
+        res.status(500).json({ error: "Contract failed to save vote" });
+      }
+      const transactionObject = {
+        date: new Date().toISOString(),
+        txnHash: transaction,
+        contribution
+      }
+      const response = await db
+        .collection("series_list")
+        .updateOne(
+          { address: address },
+          { $set: { highestContribution: contribution } },
+          { $push: { transactionList: transactionObject } }
+        )
+      res.status(200).json({ response: response, message: "Success! New vote saved" });
+      break
+    default:
+      res.setHeader('Allow', ['GET', 'POST'])
+      res.status(405).end(`Method ${method} Not Allowed`)
   }
 }
