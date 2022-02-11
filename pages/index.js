43e7f768c2adb46fcfd8abb278edb67ae707f540
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-02-11 03:18:58 -0500

    Add UserInfoContract back

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import UserInfoContext from "../src/context/UserInfoContext";
 import ContractAddressesContext from "../src/context/ContractAddressesContext";
 import RootLayout from "../src/components/_layouts/RootLayout";
 import UserSetup from "../src/components/UserSetup";
@@ -8,12 +9,12 @@ import UserProfile from "../src/components/UserProfile";
 import TransactionTable from "../src/components/TransactionTable";
 import {
   Container,
-  Flex,
   Grid,
   GridItem
 } from '@chakra-ui/react';
 
 export default function Home({ greeterAddress, simpleAuctionAddress, customVotingAddress }) {
+  const [userInfo, setUserInfo] = useState({});
   const [setupComplete, setSetupComplete] = useState(false);
   function handleAccountsChanged() {
     // Reloads account after account change
@@ -26,43 +27,45 @@ export default function Home({ greeterAddress, simpleAuctionAddress, customVotin
   }, [])
 
   return (
-    <ContractAddressesContext.Provider value={{ greeterAddress, simpleAuctionAddress, customVotingAddress }}>
-      <RootLayout>
-        {
-          // Go through checks for user to setup MetaMask, connect account, and write username
-          // Otherwise, everything is loaded and render application
-          !setupComplete
-            ? (
-              <UserSetup setSetupComplete={setSetupComplete} />
-            )
-            : (
-              <Container h={'100%'} maxW='container.lg'>
-                <Grid
-                  h={'100%'}
-                  templateRows='repeat(4, 1fr)'
-                  templateColumns='repeat(5, 1fr)'
-                  gap={8}
-                >
-                  <GridItem colSpan={1} />
-                  <GridItem colSpan={3}>
-                    <VotingCountdown />
-                  </GridItem>
-                  <GridItem colSpan={1} />
-                  <GridItem colSpan={2}>
-                    <LeaderTable />
-                  </GridItem>
-                  <GridItem colSpan={3}>
-                    <UserProfile />
-                  </GridItem>
-                  <GridItem colSpan={5}>
-                    <TransactionTable />
-                  </GridItem>
-                </Grid>
-              </Container>
-            )
-        }
-      </RootLayout>
-    </ContractAddressesContext.Provider>
+    <UserInfoContext.Provider value={{ userInfo, setUserInfo }}>
+      <ContractAddressesContext.Provider value={{ greeterAddress, simpleAuctionAddress, customVotingAddress }}>
+        <RootLayout>
+          {
+            // Go through checks for user to setup MetaMask, connect account, and write username
+            // Otherwise, everything is loaded and render application
+            !setupComplete
+              ? (
+                <UserSetup setSetupComplete={setSetupComplete} />
+              )
+              : (
+                <Container h={'100%'} maxW='container.lg'>
+                  <Grid
+                    h={'100%'}
+                    templateRows='repeat(4, 1fr)'
+                    templateColumns='repeat(5, 1fr)'
+                    gap={8}
+                  >
+                    <GridItem colSpan={1} />
+                    <GridItem colSpan={3}>
+                      <VotingCountdown />
+                    </GridItem>
+                    <GridItem colSpan={1} />
+                    <GridItem colSpan={2}>
+                      <LeaderTable />
+                    </GridItem>
+                    <GridItem colSpan={3}>
+                      <UserProfile />
+                    </GridItem>
+                    <GridItem colSpan={5}>
+                      <TransactionTable />
+                    </GridItem>
+                  </Grid>
+                </Container>
+              )
+          }
+        </RootLayout>
+      </ContractAddressesContext.Provider>
+    </UserInfoContext.Provider>
   )
 }
 
