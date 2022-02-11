7e61ad61c1bf51ea09b9c2ec3ead8debc3d9c9c9
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-02-11 05:22:33 -0500

    Update contractUtils.js

--- src/contractUtils.js
+++ src/contractUtils.js
@@ -6,6 +6,15 @@
 import { ethers } from 'ethers'
 import CustomVoting from "./artifacts/contracts/CustomVoting.sol/CustomVoting.json";
 
+// A Web3Provider wraps a standard Web3 provider, which is
+// what MetaMask injects as window.ethereum into each page
+
+// The MetaMask plugin also allows signing transactions to
+// send ether and pay to change state within the blockchain.
+// For this, you need the account signer...
+// const signer = provider.getSigner()
+
+
 export async function isVoter(contractAddress, address) {
   const provider = new ethers.providers.Web3Provider(window.ethereum);
   const contract = new ethers.Contract(contractAddress, CustomVoting.abi, provider);
